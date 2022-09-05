7b6ab7c81b4de6240c6fe75c9f9c195004c40367
MdsiddiqueR <111884247+MdsiddiqueR@users.noreply.github.com>
2022-09-05 20:17:01 +0530

    Update client.c
    Commands Added

--- C program/Socket/client.c
+++ C program/Socket/client.c
@@ -15,6 +15,7 @@ int main(){
   char buffer[1024];
   int n;
 
+  //Creating socket
   sock = socket(AF_INET, SOCK_STREAM, 0);
   if (sock < 0){
     perror("[-]Socket error");
@@ -22,23 +23,28 @@ int main(){
   }
   printf("[+]TCP server socket created.\n");
 
+  //Prepare the sockaddr_in structure
   memset(&addr, '\0', sizeof(addr));
   addr.sin_family = AF_INET;
   addr.sin_port = port;
   addr.sin_addr.s_addr = inet_addr(ip);
 
+  //Connect to remote server
   connect(sock, (struct sockaddr*)&addr, sizeof(addr));
   printf("Connected to the server.\n");
 
+  //Send some data
   bzero(buffer, 1024);
   strcpy(buffer, "HELLO, THIS IS CLIENT.");
   printf("Client: %s\n", buffer);
   send(sock, buffer, strlen(buffer), 0);
 
+  //Receive a reply from the server
   bzero(buffer, 1024);
   recv(sock, buffer, sizeof(buffer), 0);
   printf("Server: %s\n", buffer);
 
+  //closing server
   close(sock);
   printf("Disconnected from the server.\n");
 
