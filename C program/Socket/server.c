4f7501f31061ae15ab688ea37f77517f29f62092
MdsiddiqueR <111884247+MdsiddiqueR@users.noreply.github.com>
2022-09-05 20:17:31 +0530

    Update server.c
    Command Added

--- C program/Socket/server.c
+++ C program/Socket/server.c
@@ -15,6 +15,7 @@ int main(){
   char buffer[1024];
   int n;
 
+  //Creating TCP socket
   server_sock = socket(AF_INET, SOCK_STREAM, 0);
   if (server_sock < 0){
     perror("[-]Socket error");
@@ -22,35 +23,42 @@ int main(){
   }
   printf("[+]TCP server socket created.\n");
 
+  //Prepare the sockaddr_in structure
   memset(&server_addr, '\0', sizeof(server_addr));
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = port;
   server_addr.sin_addr.s_addr = inet_addr(ip);
-
+  
+  // binding
   n = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
   if (n < 0){
     perror("[-]Bind error");
     exit(1);
   }
   printf("[+]Bind to the port number: %d\n", port);
-
+  
+  //Listening
   listen(server_sock, 5);
   printf("Listening...\n");
 
+  //Accept and incoming connection
   while(1){
     addr_size = sizeof(client_addr);
     client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &addr_size);
     printf("[+]Client connected.\n");
 
+    //receving data from client
     bzero(buffer, 1024);
     recv(client_sock, buffer, sizeof(buffer), 0);
     printf("Client: %s\n", buffer);
 
+    //Reply to the client
     bzero(buffer, 1024);
     strcpy(buffer, "HI, THIS IS SERVER. HAVE A NICE DAY!!!");
     printf("Server: %s\n", buffer);
     send(client_sock, buffer, strlen(buffer), 0);
 
+    //closing connection
     close(client_sock);
     printf("[+]Client disconnected.\n\n");
 
